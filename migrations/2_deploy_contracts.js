2f819b3a0ef57c4901ef659abd3e02112c497147
danilo.mendez@gmail.com <mos@aico1>
2018-10-11 18:24:51 -0400

    Add VTO token holders

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 var ValstoToken = artifacts.require("ValstoToken.sol");
 
-//owner 0xB44a5ee07EC72f99B63a69BC470B94d6d21937d8
-var merchants = '0xEf0567915b173d0bbdd172F97F8432f0EaF15C06'; //4
-var team = '0xa96C6Cbdcda8Ef934402F22bBf76A30182FA36bd'; //5
-var contractWallet = '0x4376cad798397464E49A97e927768A353CbE96D5'; //8
+
+var merchants = '0xfd8e8c2c48a0f03722a128889cfea3bbfdc31595'; 
+var team = '0x22baacd51961c826f9f2da8976f9e7b82c057769'; 
+var contractWallet = '0x8c753cbc00ef504322a3b0f1cd326580d50f6497'; 
 
 module.exports = function(deployer) {
   deployer.deploy(ValstoToken, merchants, team, contractWallet).then(function(){})
